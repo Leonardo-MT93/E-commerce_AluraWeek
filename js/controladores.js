c812ce04a0154560f8e787a84b198c4b20c69535
Leonardo Tolaba <leonardotolaba.20@gmail.com>
2022-05-10 20:47:24 -0300

    Add server Heroku

--- js/controladores.js
+++ js/controladores.js
@@ -1,8 +1,8 @@
 
-const listaTarjetas = () => fetch("http://localhost:3000/profile").then(respuesta => respuesta.json());
+const listaTarjetas = () => fetch("https://aluraweek.herokuapp.com/profile").then(respuesta => respuesta.json());
 
 const crearTarjeta = async(Seccion, Imagen, Product, Price, Desc, id) => {
-    return fetch ('http://localhost:3000/profile', {
+    return fetch ('https://aluraweek.herokuapp.com/profile', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json'},
         body: JSON.stringify({Seccion, Imagen, Product, Price, Desc, id})
@@ -10,16 +10,16 @@ const crearTarjeta = async(Seccion, Imagen, Product, Price, Desc, id) => {
 }
 
 const eliminarTarjeta = (id) => {
-    return fetch(`http://localhost:3000/profile/${id}`, {
+    return fetch(`https://aluraweek.herokuapp.com/profile/${id}`, {
         method: "DELETE"
     });
 }
 const buscarTarjeta = (id) => {
-    return fetch(`http://localhost:3000/profile/${id}`).then((respuesta) => respuesta.json());
+    return fetch(`https://aluraweek.herokuapp.com/profile/${id}`).then((respuesta) => respuesta.json());
 }
 
 const actualizarTarjeta = (Seccion, Imagen, Product, Price, Desc, id) => {
-    return fetch(`http://localhost:3000/profile/${id}`, {
+    return fetch(`https://aluraweek.herokuapp.com/profile/${id}`, {
         method: "PUT",
         headers: {
             "Content-Type": "application/json"
